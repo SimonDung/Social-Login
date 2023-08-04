97b7b426f737e8d85a9b65491424464920e6d1c9
SikePoop <sike20022020@gmail.com>
2023-08-04 20:33:53 +0700

    add oauth2

---
+++ oauth2/src/main/java/com/example/oauth2/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.oauth2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2Login(auth -> auth.permitAll());
+        return http.build();
+    }
+}
